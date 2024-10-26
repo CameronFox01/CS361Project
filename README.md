577566d01b0124d82f45e366075b057c1f375104
CameronFox01 <cameron22401@icloud.com>
2024-10-25 21:14:58 -0600

    I got Main set up. And have worked on the README.md please check it out and let me know what your thoughts are on it.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # CS361Project
 
+## Project Designed by Cameron Fox, Ashley Elkins, Brandon Something, and Reece Something.
 
----
+# Bugs 
+Put known bugs here 
+
+# Working on
+Put what is currently being built
+
+# Need help on
+stuck on a problem? Reach out here
+
+# The people who worked on specific parts are 
+Example (But also probably will stay, how do we exactly want this filled out?)
+Cameron Fox, built the main function.
--- src/src/Main.java
+++ src/src/Main.java
@@ -1,6 +1,58 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * The File is read in as a command line argument.
+ */
 public class Main {
+    private static  String[][] fileArray;
     public static void main(String[] args) {
-        System.out.println("Hello Bitchies!");
-        System.out.println("what the heck");
+        List<String> fileString = new ArrayList<>();
+        try {
+            File file = new File(args[0]);
+            Scanner sc = new Scanner(file);
+            while (sc.hasNext()){
+                fileString.add(sc.nextLine());
+            }
+
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        fileArray = to2DArray(fileString);
+
+        // Print the 2D array to verify
+        for (String[] row : fileArray) {
+            for (String element : row) {
+                System.out.print(element);
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * This is turning the file into  2d Array, separated by each character.
+     * @param file
+     * @return
+     */
+    private static String[][] to2DArray(List<String> file) {
+        String[][] result = new String[file.size()][];
+        for (int i = 0; i < file.size(); i++) {
+            String line = file.get(i);
+            result[i] = new String[line.length()];
+            for (int j = 0; j < line.length(); j++) {
+                result[i][j] = String.valueOf(line.charAt(j));
+            }
+        }
+        return result;
+    }
+
+    /**
+     * method to get fileArray in another file.
+     * @return
+     */
+    public String[][] getFileArray(){
+        return fileArray;
     }
 }
