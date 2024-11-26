cd097c2deca5f6dd1ecc2673f0431fc0d2e791c6
FructoseJuice <116052103+FructoseJuice@users.noreply.github.com>
2024-11-25 19:55:04 -0700

    Update DFS.java

--- src/src/Algorithms/DFS.java
+++ src/src/Algorithms/DFS.java
@@ -6,16 +6,24 @@ import util.*;
 
 public class DFS {
     private static int estimatedMaxPathWeight;
+    private static Path minPath;
 
     public static Path findPath(Vertex startVertex, int numTargets) {
         estimatedMaxPathWeight = Integer.MAX_VALUE;
-        return dfs(1, numTargets, startVertex, new Set<>(), new Path(1));
+        minPath = null;
+
+        dfs(1, numTargets, startVertex, new Set<>(), new Path(1));
+
+        Path pathFound = minPath;
+        minPath = null;
+
+        return pathFound;
     }
 
-    private static Path dfs(int currDepth, int numTargets, Vertex curr, Set<Vertex> visited, Path path) {
-        if (currDepth > estimatedMaxPathWeight) return null;
+    private static void dfs(int currDepth, int numTargets, Vertex curr, Set<Vertex> visited, Path path) {
+        if (currDepth > estimatedMaxPathWeight) return;
 
-        if (!visited.tryAdd(curr)) return null;
+        if (!visited.tryAdd(curr)) return;
 
         //Update path
         path.add(curr);
@@ -24,21 +32,16 @@ public class DFS {
             numTargets--;
 
             if (numTargets == 0) {
-                //Save new good path
-                Path foundPath = new Path(path);
-                foundPath.setFullPath(true);
+                tryUpdateMinPath(path);
 
                 //Backtrack
                 visited.remove(curr);
                 path.removeLast();
-
-                return foundPath;
+                return;
             }
         }
 
-        Path minPath = null;
         int oldWeight = path.getWeight();
-
         Set<WeightedEdge<Vertex, Vertex>> adjacentVertices = Main.weightedGraph.verticesAdjacentTo(curr);
 
         //Continue dfs
@@ -48,29 +51,23 @@ public class DFS {
 
             //Adjust weight
             path.setWeight(oldWeight + edge.getWeight());
-            Path responsePath = dfs(currDepth + edge.getWeight(), numTargets, otherVertex, visited, path);
-
-            //Don't consider path if it's null or not a found path
-            if (responsePath == null || !responsePath.isFullPath()) continue;
-
-            //Track the minimum path found
-            if (minPath == null || minPath.getWeight() > responsePath.getWeight()) {
-                minPath = responsePath;
-                updateEstimate(minPath.getWeight());
-            }
+            dfs(currDepth + edge.getWeight(), numTargets, otherVertex, visited, path);
         }
 
         //Backtrack
         visited.remove(curr);
         path.removeLast();
-
-        //Return min found path
-        return minPath;
     }
 
-    private static void updateEstimate(int newDepth) {
-        if (newDepth < estimatedMaxPathWeight) {
-            estimatedMaxPathWeight = newDepth;
+    private static void tryUpdateMinPath(Path newPath) {
+        if (minPath == null) {
+            minPath = new Path(newPath);
+            estimatedMaxPathWeight = newPath.getWeight();
+        } else {
+            if (newPath.getWeight() < minPath.getWeight()) {
+                minPath = new Path(newPath);
+                estimatedMaxPathWeight = newPath.getWeight();
+            }
         }
     }
 }
